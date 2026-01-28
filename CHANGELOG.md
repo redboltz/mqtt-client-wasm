408ed47b8b423aa6d64aa74134bd1c5e59d95c36
Takatoshi Kondo <redboltz@gmail.com>
2026-01-28 19:26:04 +0900

    Remove output files.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.2.2
+
+* Remove generated files from the repository. #18
+
 # 0.2.1
 
 * Add direct browser support. #17
--- pkg-bundler/mqtt_client_wasm.js
+++
@@ -1,9 +0,0 @@
-/* @ts-self-types="./mqtt_client_wasm.d.ts" */
-
-import * as wasm from "./mqtt_client_wasm_bg.wasm";
-import { __wbg_set_wasm } from "./mqtt_client_wasm_bg.js";
-__wbg_set_wasm(wasm);
-wasm.__wbindgen_start();
-export {
-    JsTransport, WasmAuthPacketV5_0, WasmConnackPacketV3_1_1, WasmConnackPacketV5_0, WasmDisconnectPacketV3_1_1, WasmDisconnectPacketV5_0, WasmMqttClient, WasmMqttConfig, WasmMqttPacket, WasmPacketType, WasmPubackPacketV3_1_1, WasmPubackPacketV5_0, WasmPubcompPacketV3_1_1, WasmPubcompPacketV5_0, WasmPublishPacketV3_1_1, WasmPublishPacketV5_0, WasmPubrecPacketV3_1_1, WasmPubrecPacketV5_0, WasmPubrelPacketV3_1_1, WasmPubrelPacketV5_0, WasmSubackPacketV3_1_1, WasmSubackPacketV5_0, WasmUnsubackPacketV3_1_1, WasmUnsubackPacketV5_0, check_version, createClientWithJsTransport, init
-} from "./mqtt_client_wasm_bg.js";
